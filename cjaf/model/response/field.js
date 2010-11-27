4a9fe10bd71d5452425688002810ac8abc1d060c
Nathan <scull7@gmail.com>
2010-11-27 01:15:04 -0800

    Add in the response parser object

---
+++ cjaf/model/response/field.js
@@ -0,0 +1,62 @@
+/**
+ * This is a response object field specification.
+ */
+
+/** JSLint declarations */
+/*global jQuery: false, cjaf: false*/
+
+(function ($, cjaf) {
+	cjaf.define('cjaf/model/response/field', [
+		
+	],
+	/**
+	 * @return {cjaf.Model.Response.Field}
+	 */
+	function () {
+		cjaf.Model.Response.Field	= function (name, is_required, alt_names) {
+			/**
+			 * This is the field name that the CJAF model object will expect.
+			 * @type {string}
+			 */
+			this.name	= name;
+			/**
+			 * Is this a required field?
+			 * @type {boolean}
+			 */
+			this.required	= is_required ? true : false;
+			/**
+			 * This is a list of alternate names for this field.  This is
+			 * used to check the service response in case the name does not
+			 * exist in the response. This way a model object can deal with 
+			 * non-uniform services.
+			 * @type {Array.<string>}
+			 */
+			this.alt_names	= alt_names;
+		};
+		$.extend(cjaf.Model.Response.Field.prototype, {
+			/**
+			 * Get the name of this field.
+			 * @return {string}
+			 */
+			"getName": function () {
+				return this.name;
+			},
+			/**
+			 * Is this a required field?
+			 * @return {boolean}
+			 */
+			"isRequired": function () {
+				return this.required ? true : false;
+			},
+			/**
+			 * Get all of the alternate names.
+			 * @return {Array.<string>}
+			 */
+			"getAlternateNames": function () {
+				return this.alt_names;
+			}
+		});
+		
+		return cjaf.Model.Response.Field;
+	});
+}(jQuery, cjaf));
---
+++ cjaf/model/response/parser.js
@@ -0,0 +1,102 @@
+/**
+ * This is a parser/sanity checking object for service response objects.
+ */
+
+/** JSLint defines */
+/*global console: false, jQuery: false, cjaf: false*/
+
+(function ($, cjaf) {
+	cjaf.define('cjaf/model/response/parser', [
+		'cjaf/collection',
+		'cjaf/model',
+		'cjaf/model/response/field'
+	],
+	/**
+	 * @param {cjaf.Collection} Collection
+	 * @param {cjaf.Model} Model
+	 * @param {cjaf.Model.Response.Field} Field
+	 * @return {cjaf.Response.Parser}
+	 */
+	function (Collection, Model, Field) {
+		if (!cjaf.Model.hasOwnProperty('Response')) {
+			cjaf.Model.Response	= {};
+		}
+		
+		cjaf.Model.Response.Parser	= function (fields) {
+			var field_name, field_rec, field_obj, required, alts;
+			
+			this.fields	= null;
+			
+			if (!(fields instanceof Collection)) {
+				this.fields	= new Collection();
+				
+				for (field_name in fields) {
+					if (fields.hasOwnProperty(field_name)) {
+						field_rec	= fields[field_name];
+						
+						if (!(field_rec instanceof Field)) {
+							required	= field_rec.hasOwnProperty('required') ? field_rec.required : false;
+							alts		= field_rec.hasOwnProperty('alts') ? field_rec.alts : [];
+							
+							field_obj	= new Field(field_name, required, alts);
+						} else {
+							field_obj	= field_rec;
+						}
+						
+						this.fields.add(field_obj);
+					}
+				}
+			} else {
+				this.fields	= fields;
+			}
+		};
+		$.extend(cjaf.Model.Response.Parser.prototype, {
+			/**
+			 * This will ensure that the given response object contains the
+			 * proper data members to create a model object.  It will return
+			 * an object with the proper data members set if the given 
+			 * object is valid.
+			 * 
+			 * @param {Object} response
+			 * @return {Object}
+			 */
+			parse: function (response) {
+				var field_list	= this.fields,
+					return_obj	= {},
+					field, value, required, alt, alt_iter, has_field;
+					
+				while (field_list.hasNext()) {
+					value	= null;
+					field	= field_list.getNext();
+					
+					if (!response.hasOwnProperty(field.getName())) {
+						alt_iter	= field.getAlternateNames().getIter();
+						
+						while (alt_iter.hasNext()) {
+							alt	= alt_iter.getNext();
+							
+							if (response.hasOwnProperty(alt)) {
+								value	= response[alt];
+							}
+						}
+					} else {
+						value	= response[field.getName()];
+					}
+					
+					if (!value && field.isRequired()) {
+						if (console) {
+							console.log('missing field: ' + field);
+							console.log(response);
+						}
+						$.error('Response object not valid. Missing "' +
+									field + '" field.');
+					}
+					
+					return_obj[field.getName()]	= value;
+				}
+				
+				return return_obj;
+			}
+		});
+	});
+}(jQuery, cjaf));
