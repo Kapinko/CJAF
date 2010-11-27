b40a2b4b59b85f96f84b31ad8b5b5b281c8d79a4
Nathan <scull7@gmail.com>
2010-11-27 00:37:10 -0800

    Add a basic collection object and a base model collectiion object

---
+++ cjaf/collection.js
@@ -0,0 +1,120 @@
+/**
+ * This is a basic collection object.
+ */
+
+/** JSLint Declarations */
+/*global jQuery: false, cjaf: false*/
+
+(function ($, cjaf) {
+	cjaf.define('cjaf/collection', [
+		'lib/plugins/Array/iterator'
+	],
+	/**
+	 * @return {cjaf.Collection}
+	 */
+	function () {
+		cjaf.Collection	= function (item_filter) {
+			/**
+			 * This is the array of items that we're providing an interface to.
+			 * @type {Array.<*>}
+			 */
+			this.collection	= [];
+			/**
+			 * This is the array iterator currently in use.
+			 * @type {ArrayIterator}
+			 */
+			this.iterator	= null;
+			/**
+			 * This is a filter function that will be applied to items as they
+			 * are added to this collection.  It must return a boolean value
+			 * if the item is allowed to be added or not. If a filter is not
+			 * provided then all requested items will be allowed.
+			 * @type {function(*):boolean}
+			 */
+			this.item_filter	= function (item) {
+				return true;
+			};
+				
+			if (typeof item_filter === 'function') {
+				this.item_filter	= item_filter;
+			}
+								
+			this.init();
+		};
+		$.extend(cjaf.Collection.prototype, {
+			/**
+			 * An initialization method so that any decendent objects can
+			 * perform initialization tasks.
+			 */
+			"init": function () {
+				
+			},
+			/**
+			 * Add an item to this collection
+			 * @param {*} item
+			 * @return {cjaf.Collection}
+			 * @throws {string}
+			 */
+			"add": function (item) {
+				if (!this.item_filter(item)) {
+					throw "Unable to add item to collection.";
+				}
+				
+				this.collection.push(item);
+				
+				if (this.iterator !== null) {
+					this.iterator	= null;
+				}
+				return this;
+			},
+			/**
+			 * Get this collection as an array.
+			 * @return {Array.<*>}
+			 */
+			"getAll": function () {
+				return this.collection;
+			},
+			/**
+			 * How many items to we have in this collection?
+			 * @return {number}
+			 */
+			"getCount": function () {
+				return this.collection.length;
+			},
+			/**
+			 * Does this collection have an object at the next index?
+			 * @return {boolean}
+			 */
+			"hasNext": function () {
+				return this.getIterator().hasNext();
+			},
+			/**
+			 * Get the next item in this collection.
+			 * @return {*}
+			 */
+			"getNext": function () {
+				return this.getIterator().getNext();
+			},
+			/**
+			 * Reset the iterator for this collection.
+			 * @return {cjaf.Collection}
+			 */
+			"reset": function () {
+				this.iterator	= null;
+				return this;
+			},
+			/**
+			 * Get an iterator to iterate through this collection's objects.
+			 * @return {ArrayIterator}
+			 */
+			getIterator: function () {
+				if (this.iterator === null) {
+					this.iterator	= this.collection.getIter();
+				}
+				return this.iterator;
+			}
+		});
+		
+		return cjaf.Collection;
+	});
+}(jQuery, cjaf));
---
+++ cjaf/model/collection.js
@@ -0,0 +1,109 @@
+/**
+ * This is an object to hold a collection of model objects.
+ */
+
+/** JSLint Declarations */
+/*global jQuery: false, cjaf: false*/
+/*jslint newcap: false*/
+
+(function ($, cjaf) {
+	cjaf.define('cjaf/model/collection', [
+		'cjaf/collection',
+		'cjaf/model',
+		'cjaf/model/repsonse/parser'
+	],
+	/**
+	 * @param {cjaf.Model.Response.Parser} ResponseParser
+	 * @return {cjaf.Model.Collection}
+	 */
+	function (Collection, Model, ResponseParser) {
+		cjaf.Model.Collection	= Collection.extend({
+			/**
+			 * Thses are the fields that should/must be in a server response
+			 * that claims to contain a collection.
+			 * @type {Object.<string, *>}
+			 */
+			fields: {
+				'collection': {
+					'required': false,
+					'alts': ['items']
+				},
+				'count': {
+					'required': false,
+					'alts': ['total']
+				}
+			},
+			/**
+			 * An intialization function that is meant to be overridden by any
+			 * child objects.
+			 */
+			"init": function () {
+				/**
+				 * This is the total number of model objects that the service used
+				 * to retrieve them says we should have.
+				 * @type {number}
+				 */
+				this.serviceTotal	= null;
+				/**
+				 * This is the prototype object for all the objects that we 
+				 * are collecting.
+				 * @type {Object}
+				 */
+				this.containedPrototype	= null;
+			},
+			/**
+			 * Add the given model object to this collection.
+			 * @param {cjaf.Model.Abstract} model
+			 * @return {cjaf.Model.Collection.Base}
+			 * @throw {Exception}
+			 */
+			"add": function (model) {
+				if (this.containedPrototype === null) {
+					this.containedPrototype	= model;
+					
+				} else if (!(model instanceof this.containedPrototype)) {
+					$.error('Given model is not an instance of ' + 
+						this.containedPrototype);
+				}
+				
+				return Collection.prototype.add.apply(this, arguments);
+			},
+			/**
+			 * Get the number of model objects that the service we used to 
+			 * retrieve them thinks should be in this collection.
+			 * @return {number}
+			 */
+			"getServiceTotal": function () {
+				return this.serviceTotal;
+			},
+			/**
+			 * Make sure that this collection object is not malformed.
+			 * @throws {Exception}
+			 */
+			"validate": function () {
+				//Nothing to validate for the base collection.
+			}
+		});
+		
+		/**
+		 * Get a collection object from the given response object.
+		 * @param {Object.<string,*>} response
+		 * @param {XMLHttpRequest} XMLHttpRequest
+		 * @return {cjaf.Model.Collection}
+		 */
+		cjaf.Model.Collection.wrap	= function (response, XMLHttpRequest) {
+			var collection	= new this(),
+				parser		= new ResponseParser(collection.fields);
+				
+			response		= parser.parse(response);
+			
+			if (response.hasOwnProperty('count')) {
+				collection.serviceTotal	= response.count;
+			}
+			
+			return collection;
+		};
+		
+		return cjaf.Model.Collection;
+	});
+}(jQuery, cjaf));
