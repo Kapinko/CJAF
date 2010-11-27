55aed3abb0a53f71d0c4d2ba744a19e28f89c32c
Nathan <scull7@gmail.com>
2010-11-27 00:43:18 -0800

    change the extend plugin to attach to the Object prototype. This way we can just call parent.extend()

--- cjaf/object.js
+++ cjaf/object.js
@@ -13,18 +13,17 @@
 	],
 	function () {
 		//Only add the "extend" if it does not currently exist.
-		if (Object.extend !== 'function') {
+		if (Object.prototype.extend !== 'function') {
 			/**
 			 * This is a function that provides syntatic suger so that we can
 			 * extend objects in a more readable way.
 			 * 
-			 * @param {Object} parent
 			 * @param {Object} child_proto - this should be an object literal that
 			 *					defines the child object's prototype extensions.
 			 * @return {Object}
 			 */
-			Object.extend	= function (parent, child_proto) {
-				var child	= Object.create(parent);
+			Object.prototype.extend	= function (child_proto) {
+				var child	= Object.create(this);
 				$.extend(child.prototype, child_proto);
 				
 				return child;
