554aa1c16e1aa2637007548004fb710ae711a27b
Nathan <scull7@gmail.com>
2010-12-02 09:57:17 -0800

    add doc files for the menu system

---
+++ cjaf/widget/helper/menu/renderer/dock.js
@@ -0,0 +1,82 @@
+/**
+ * This is a menu renderer that will render a menu as an OSX style dock.
+ */
+/** JSLint Declarations */
+/*global jQuery: false, cjaf: false */
+
+(function ($, cjaf) {
+	cjaf.define('cjaf/widget/helper/menu/renderer/dock', [
+		'cjaf/widget/helper/menu/renderer',
+		'jQuery/jquery.jqDock'
+	],
+	/**
+	 * @param {cjaf.Widget.Helper.Menu.Renderer} Renderer
+	 * @return {cjaf.Widget.Helper.Menu.Renderer.Dock}
+	 */
+	function (Renderer) {
+		Renderer.Dock	= cjaf.Class.extend(Renderer, {
+			/**
+			 * This function will render the menu itself.
+			 * @param {Menu} menu
+			 * @return {jQuery}
+			 */
+			"renderMenu": function (menu) {
+				var div	= $('<div>');
+				div.addClass(this.menu_class)
+					.attr('id', menu.getId());
+					
+				return div;
+			},
+			/**
+			 * This function will render a given menu item.  Menu items will be
+			 * placed into the menu in the order which they are given.
+			 * @param {MenuItem} menu_item
+			 * @return {jQuery}
+			 */
+			"renderMenuItem": function (menu_item) {
+				var title	= menu_item.getTitle(),
+				ref			= menu_item.getRef(), item;
+
+				if (!title) {
+					title	= menu_item.getId();
+				}
+				
+				if (ref) {
+					item	= $('<a>');
+					item.attr('href', '#' + ref)
+						.attr('title', title);
+						
+					$('<img>').attr('src', menu_item.getImageUrl())
+							.attr('alt', '')
+							.attr('title', '')
+							.appendTo(item);
+					
+				} else {
+					item	= $('<img>');
+					item.attr('src', menu_item.getImageUrl())
+						.attr('alt', '')
+						.attr('title', title);
+				}
+				return item;
+			},
+			/**
+			 * This is a hook so that child classes can do any necessary 
+			 * configuration after the menu has been created but before
+			 * we pass the menu of to the user.
+			 * @param {jQuery} container
+			 * @oaram {jQuery} menu_html
+			 * @param {Menu} menu
+			 * @return {Renderer}
+			 */
+			"postRenderHook": function (container, menu_html, menu) {
+				var dockOptions	= {
+					"align": 'left',
+					"labels": true
+				};
+				menu_html.jqDock(dockOptions);
+			}
+		});
+		
+		return Renderer.Dock;
+	});
+}(jQuery, cjaf));
