09c8370828d838b00c50c0b5f3d54d8bdf5b7807
Nathan Sculli <nathan.sculli@ecommlink.com>
2010-12-07 11:52:04 -0800

    Add the a test of the +1 dispatchers per page.

--- test/js/test/widget/page/index.js
+++ test/js/test/widget/page/index.js
@@ -7,9 +7,12 @@
 
 (function ($, cjaf) {
 	cjaf.define('test/widget/page/index', [
-		'cjaf/widget/form'
+		'cjaf/widget/helper/event',
+		'cjaf/widget/dispatcher',
+		'cjaf/widget/form',
+		'test/widget/page/test'
 	],
-	function () {
+	function (EventHelper) {
 		$.widget('cjaf.test_page_index', {
 			/**
 			 * These are the available options and their defaults for this
@@ -28,8 +31,33 @@
 			 * The initialization function for this widget.
 			 */
 			_create: function () {
-				var o	= this.options;
-				this.element.html(this._view(o.locale));
+				var o		= this.options,
+					el		= this.element,
+					content;
+
+				el.html(this._view(o.locale));
+				content	= this.element.find('#content-section')
+
+				el.dispatcher({
+					defaultPage:	"test",
+					pages:			{
+						"test": "test_page_test"
+					},
+					"contentElement": content,//Antonio Banderas
+					"transitionOut": function (element, callback) {
+						element.effect("explode", {"mode": "hide"}, 500, callback);
+					},
+					"transitionIn": function (element, callback) {
+						element.effect("fade", {"mode": "show"}, 500, callback);
+					}
+				});
+				el.dispatcher('render');
+
+				el.find('.avmenu li a').click(function (event) {
+					el.trigger(EventHelper.dispatcher.content.change, [{id: 'test'}]);
+					event.stopPropagation();
+					event.preventDefault();
+				});
 			}
 		});
 	});
---
+++ test/js/test/widget/page/test.js
@@ -0,0 +1,21 @@
+/**
+ * This is a test sub page.
+ */
+/*global jQuery: false, cjaf: false*/
+
+(function ($, cjaf) {
+	cjaf.define('cjaf/test/widget/page/test', [],
+	/**
+	 * @param {cjaf.Widget.Helper.Event} EventHelper
+	 */
+	function () {
+		$.widget('cjaf.test_page_test', {
+			options: {
+
+			},
+			_create: function () {
+				this.element.html(this._view({}));
+			}
+		});
+	});
+}(jQuery, cjaf));
